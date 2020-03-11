9afce742245e4c207b9b0e3f51363fa98740792a
Usuario <Usuario@PC003>
2020-03-11 17:20:24 -0500

    W.Levita - 11/03/2020

---
+++ WebERP.PeruMoney/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebERP.PeruMoney
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include(
+                "~/Content/vendor/fontawesome-free/css/all.min.css",
+                "~/Content/css/sb-admin-2.min.css"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/vendor/jquery-easing/jquery.easing.min.js",
+                "~/Content/js/sb-admin-2.min.js",
+                "~/Content/vendor/chart.js/Chart.min.js",
+                "~/Content/js/demo/chart-area-demo.js",
+                "~/Content/js/demo/chart-pie-demo.js"));
+        }
+    }
+}
---
+++ WebERP.PeruMoney/Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebERP.PeruMoney.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
